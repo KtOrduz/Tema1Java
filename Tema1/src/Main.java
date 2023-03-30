376d3d9c4d8137ad5b1f153b8c3e2bb8270d8e44
Katherine Orduz <kathoa63@gmail.com>
2023-03-29 22:12:38 -0500

    tema1

---
+++ Tema1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numero=1;
+        long cc=1234567000;
+        double porcentaje= 121.25;
+
+        boolean soleado=true;
+
+        String nombre="Kt";
+
+        System.out.println(numero);
+        System.out.println(cc);
+        System.out.println(porcentaje);
+        System.out.println(soleado);
+        System.out.println(nombre);
+    }
+}
